5e177c7c790feec73ea896875ba32a5edfdab3ea
Brian Whitney <brian.whitney@alleninstitute.org>
2025-03-21 13:31:59 -0700

    remove ex file

--- test.py
+++
@@ -1,7 +0,0 @@
-import bioio
-import bioio_lif
-
-path = "/Users/brian.whitney/Desktop/Repos/bioio-lif/bioio_lif/tests/resources/AICS-102_CofA_FinalBank.lif"
-
-image = bioio.BioImage(path)
-image.manifest
